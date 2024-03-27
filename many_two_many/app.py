60516e15ea57e616608e590adbcb36c69feebec3
drxaslam <drxaslam7890@gmail.com>
2024-03-27 11:29:35 +0530

    commit for flask mapping

---
+++ many_two_many/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
+
+db=SQLAlchemy(app)
+user_page=db.Table('user_page',
+    db.Column('user_id',db.Integer,db.ForeignKey('user.id')),
+    db.Column('page_id',db.Integer,db.ForeignKey('page.id')),)
+class User(db.Model):
+
+    id= db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100),nullable=False)
+    following=db.relationship('Page',secondary=user_page,backref='followers')
+    def __repr__(self) -> str:
+        return f'<User:{self.name}>'
+
+
+class Page(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(10))
+    def __repr__(self) -> str:
+        return f'<page:{self.name}>'
+
+if __name__=="__main__":
+    app.run(debug=True)
